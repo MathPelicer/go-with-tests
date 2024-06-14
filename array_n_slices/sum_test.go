ad3d2c7b71e5ce2ce290fcfda6f9120c4383c778
MathPelicer <matheuspelicer58@gmail.com>
2024-06-14 17:18:10 -0300

    feat: more tdd with slices and arrays

--- array_n_slices/sum_test.go
+++ array_n_slices/sum_test.go
@@ -1,6 +1,10 @@
 package arraynslices
 
-import "testing"
+import (
+	"reflect"
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("5 number collection", func(t *testing.T) {
@@ -12,5 +16,34 @@ func TestSum(t *testing.T) {
 		if expected != result {
 			t.Errorf("Expected %d, but got %d, %v", expected, result, numbers)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	result := SumAll([]int{1, 2}, []int{0, 9})
+	expected := []int{3, 9}
+
+	if !slices.Equal(expected, result) {
+		t.Errorf("Expected %d, but got %d", expected, result)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, expected, result []int) {
+		t.Helper()
+		if !reflect.DeepEqual(expected, result) {
+			t.Errorf("Expected %d, but got %d", expected, result)
+		}
+
+	}
+	t.Run("sum some slices", func(t *testing.T) {
+		expected := []int{2, 9}
+		result := SumAllTails([]int{1, 2}, []int{0, 9})
+		checkSums(t, expected, result)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		expected := []int{0, 9}
+		result := SumAllTails([]int{}, []int{0, 9})
+		checkSums(t, expected, result)
 	})
 }
